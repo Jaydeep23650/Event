1963ffb0896300ae813953eaab520a7d3bb06156
jaydeep <jaydeepjnvmzp2002@gmail.com>
2025-02-11 06:59:52 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Event-management/", // Use your GitHub repository name
+  base: "/Event/", // Use your GitHub repository name
   plugins: [react()],
 });
